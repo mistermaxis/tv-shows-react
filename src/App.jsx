4e9f14c4c90a2effae976f80e3e08de16a7b4e9d
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-10-07 17:50:29 -0300

    Test tailwind class on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="p-6">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,7 +14,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         Click on the Vite and React logos to learn more
       </p>
     </div>
