62483e29c554b5b9c008029b768f75425c5c8354
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-10-08 02:43:57 -0300

    Fix eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
@@ -18,7 +18,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
