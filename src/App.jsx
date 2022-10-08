85f6f0387dc91e7f29ea31285831a409d4342f5e
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-10-08 19:24:49 -0300

    Update App.jsx with Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1>Vite + React</h1>
-      <div className="p-6">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route index element={<Home />}/>
+      <Route path='/show/:id' element={<Detail />}/>
+    </Routes>
   );
 }
 
